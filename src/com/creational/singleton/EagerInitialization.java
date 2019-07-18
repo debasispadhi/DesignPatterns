8bef91e81d2f3c6687fafa4361b13a830ce6a64a
DEBASIS PADHI <debasis.padhi@bt.com>
2019-07-18 18:26:42 +0530

    ed

--- src/com/creational/singleton/EagerInitialization.java
+++ src/com/creational/singleton/EagerInitialization.java
@@ -1,6 +1,6 @@
 package com.creational.singleton;
 /* Drawback : 
- * 1. This method doesn’t provide any options for exception handling.
+ * 1. This method doesnot provide any options for exception handling.
  * 2. Instance is created even though client application might not be using it.*/
 
 public class EagerInitialization {
