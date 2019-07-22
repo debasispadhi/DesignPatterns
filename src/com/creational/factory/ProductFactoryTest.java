c4f57f7a63d8fb3f25dc24fba382ee11c680a3f9
DEBASIS PADHI <debasis.padhi@bt.com>
2019-07-22 15:39:08 +0530

    Factory Desgn pattern

---
+++ src/com/creational/factory/ProductFactoryTest.java
@@ -0,0 +1,24 @@
+package com.creational.factory;
+
+public class ProductFactoryTest {
+	 public static void main(String[] args) {
+	      ProductFactory ProductFactory = new ProductFactory();
+
+	      //get an object of PSTN.
+	      Product pstnProduct = ProductFactory.getProduct("PSTN");
+	      pstnProduct.selectProduct();
+
+	      //get an object of Broadband.
+	      Product broadbandProduct = ProductFactory.getProduct("Broadband");
+	      broadbandProduct.selectProduct();
+
+	      //get an object of TV.
+	      Product tvProduct = ProductFactory.getProduct("TV");
+	      tvProduct.selectProduct();
+	      
+	      //get an object of Mobile.
+	      Product mobileProduct = ProductFactory.getProduct("Mobile");
+	      mobileProduct.selectProduct();
+	   }
+}
+
